3097e2b3693abdb2f8eff0b99af69b517acf6964
KevinSimo2022 <k.takoulo@alustudent.com>
2024-09-07 16:02:05 +0200

    ff

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
-"""
-This script defines a type-annotated function `add` that returns the sum of 
-two floats.
-"""
 
 def add(a: float, b: float) -> float:
     """
-    Adds two float numbers and returns the result.
-
-    Parameters:
-    a (float): The first float number.
-    b (float): The second float number.
-
-    Returns:
-    float: The sum of `a` and `b`.
+    Returns the sum of two float numbers.
     """
     return a + b
