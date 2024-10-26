cd1ec1a9fc155f4b31ee3cb3268ed158a08dd8d4
KevinSimo2022 <k.takoulo@alustudent.com>
2024-10-26 20:55:28 +0200

    my SQL Advanced

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- uniq users
+CREATE TABLE IF NOT EXISTS users (
+    id int auto_increment not null,
+    email varchar(255) unique not null,
+    name varchar(255),
+    primary key (id)
+)
---
+++ MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,2 @@
+-- index my names
+CREATE INDEX idx_name_first ON names (name(1));
